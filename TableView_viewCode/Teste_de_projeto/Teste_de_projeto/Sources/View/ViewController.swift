1810fe4fcb75e42285e72b2b189848621a392bbf
Tiago <tiagosdeanna@gmail.com>
2022-04-14 05:37:45 -0300

    fazendo testes

--- TableView_viewCode/Teste_de_projeto/Teste_de_projeto/Sources/View/ViewController.swift
+++ TableView_viewCode/Teste_de_projeto/Teste_de_projeto/Sources/View/ViewController.swift
@@ -2,16 +2,17 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let tableView = UITableView()
+    var tableView = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(tableView)
         tableView.dataSource = self
+        // tableView.delegate = self
         view.backgroundColor = . systemBlue
         tableView.backgroundColor = .red
         constraintsTableView()
-            }
+    }
     
     private func constraintsTableView() {
         tableView.translatesAutoresizingMaskIntoConstraints = false
@@ -21,9 +22,12 @@ class ViewController: UIViewController {
         tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
         tableView.rowHeight = 255
         tableView.register(MyCustomCell.self, forCellReuseIdentifier: "MyCell")
-
+        tableView.isScrollEnabled = true
+        tableView = UITableView(frame: .zero, style: .insetGrouped)
     }
 }
+
+
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -34,21 +38,28 @@ extension ViewController: UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
         return cell
     }
-        
+    
 }
 
 class MyCustomCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        backgroundColor = .darkGray
+        backgroundColor = .clear
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-}
-
-extension MyCustomCell: UITableViewCell {
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 200
+//    }
+    
     
 }
+
+//extension ViewController: UITableViewDelegate {
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        tableView.deselectRow(at: indexPath, animated: true)
+//    }
+//}
