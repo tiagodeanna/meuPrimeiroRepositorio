3855b22ccb78f3a6c27f0dc24bd802a1651dd4ce
Tiago <tiagosdeanna@gmail.com>
2022-04-15 12:23:41 -0300

    adicionando um texto e uma imagem a minha cell

--- TableView_viewCode/Teste_de_projeto/Teste_de_projeto/Sources/View/ViewController.swift
+++ TableView_viewCode/Teste_de_projeto/Teste_de_projeto/Sources/View/ViewController.swift
@@ -7,7 +7,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(tableView)
-        tableView.dataSource = self
+        //tableView.dataSource = self
         // tableView.delegate = self
         view.backgroundColor = . systemBlue
         tableView.backgroundColor = .red
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
         tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
         tableView.rowHeight = 255
-        tableView.register(MyCustomCell.self, forCellReuseIdentifier: "MyCell")
+       // tableView.register(MyCustomCell.self, forCellReuseIdentifier: "MyCell")
         tableView.isScrollEnabled = true
         tableView = UITableView(frame: .zero, style: .insetGrouped)
     }
@@ -29,34 +29,36 @@ class ViewController: UIViewController {
 
 
 
-extension ViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        10
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
-        return cell
-    }
-    
-}
+//extension ViewController: UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        10
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+//        cell.textLabel?.text = "Hello"
+//        cell.imageView?.image = UIImage(named: "perfil")
+//        return cell
+//    }
 
-class MyCustomCell: UITableViewCell {
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        backgroundColor = .clear
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+//}
+//
+//class MyCustomCell: UITableViewCell {
+//    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+//        super.init(style: style, reuseIdentifier: reuseIdentifier)
+//        backgroundColor = .link
+//    }
+//
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
     
 //    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
 //        return 200
 //    }
     
     
-}
+//}
 
 //extension ViewController: UITableViewDelegate {
 //    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
